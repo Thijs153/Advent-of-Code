88ae294f492044efd37a3a25aa2c729a8c170737
Thijs van der Vegt <497215@student.saxion.nl>
2022-12-03 14:34:40 +0100

    Day 3 solution

---
+++ AOC.2022/Day3.cs
@@ -0,0 +1,35 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AOC._2022;
+
+public class Day3
+{
+    private List<string> _inputs = new();
+    
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        _inputs = File.ReadAllLines("Inputs/Day3.txt").ToList();
+    }
+
+    [Test]
+    public void Part1()
+    {
+        int total = _inputs.Sum(line => 
+            GetPriority( line[..(line.Length / 2)].Intersect(line[(line.Length / 2)..]).First()));
+        
+        total.Should().Be(7795);
+    }
+    
+    [Test]
+    public void Part2()
+    {
+        int total = _inputs.Chunk(3).Sum(chunk => 
+            GetPriority(chunk[0].Intersect(chunk[1]).Intersect(chunk[2]).First()));
+
+        total.Should().Be(2703);
+    }
+
+    private static int GetPriority(char character) => char.IsLower(character) ? character - 'a' + 1 : character - 'A' + 27;
+}

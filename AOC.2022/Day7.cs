adde08efd8dc83640f03a9f9eafb28a6b2349222
Thijs153 <497215@student.saxion.nl>
2022-12-08 20:03:04 +0100

    Day 7 refactor

--- AOC.2022/Day7.cs
+++ AOC.2022/Day7.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -5,90 +6,40 @@ namespace AOC._2022;
 
 public class Day7
 {
-    private Dir _currentDir = default!;
+    private List<int> _directorySizes = default!;
 
     [OneTimeSetUp]
     public void OneTimeSetUp()
     {
-        foreach (string line in File.ReadLines("Inputs/Day7.txt"))
+        var path = new Stack<string>();
+        var sizes = new Dictionary<string, int>();
+        foreach (var line in File.ReadLines("Inputs/Day7.txt"))
         {
-            if (line == "$ ls") continue;
-            if (line.StartsWith("$ cd"))
-            {
-                if (line == "$ cd /")
-                {
-                    _currentDir = Dir.Root;
-                } 
-                else if (line == "$ cd ..")
-                {
-                    _currentDir = _currentDir.Parent;
+            if (line == "$ ls" || line.StartsWith("dir")) continue;
+            if (line == "$ cd ..") {
+                path.Pop();
+            } else if (line.StartsWith("$ cd")) {
+                path.Push(string.Join("", path)+line.Split(" ")[2]);
+            } else {
+                var size = int.Parse(line.Split(" ")[0]);
+                foreach (var dir in path) {
+                    sizes[dir] = sizes.GetValueOrDefault(dir) + size;
                 }
-                else
-                {
-                    _currentDir = GetOrCreateDir(line.Split(" ")[2]);
-                }
-            }
-            else if (line.StartsWith("dir"))
-            {
-                GetOrCreateDir(line.Split(" ")[1]);
-            }
-            else
-            {
-                _currentDir.Files.Add(new F(line.Split(" ")[1], int.Parse(line.Split(" ")[0])));
             }
         }
+        _directorySizes = sizes.Values.ToList();
     }
     
     [Test]
     public void Part1()
     {
-        GetTotalDirs(Dir.Root).Should().Be(1555642);
+        _directorySizes.Where(size => size < 100_000).Sum().Should().Be(1555642);
     }
-
+    
     [Test]
     public void Part2()
     {
-        var freeSpace = 70000000 - Dir.Root.Size();
-        var spaceToFree = 30000000 - freeSpace;
-
-        GetAllDirs(Dir.Root)
-            .OrderBy(d => d.Size())
-            .First(x => x.Size() > spaceToFree)
-            .Size()
+        _directorySizes.Where(size => size + (70_000_000 - _directorySizes.Max()) >= 30_000_000).Min()
             .Should().Be(5974547);
     }
-
-    private static int GetTotalDirs(Dir d) => (d.Size() <= 100000 ? d.Size() : 0) + d.Dirs.Sum(GetTotalDirs);
-
-    private static IEnumerable<Dir> GetAllDirs(Dir d)
-    {
-        foreach (Dir subDir in d.Dirs.SelectMany(GetAllDirs))
-        {
-            yield return subDir;
-        }
-
-        yield return d;
-    }
-
-    private Dir GetOrCreateDir(string dirName)
-    {
-        Dir? newDir = _currentDir.Dirs.FirstOrDefault(x => x.Name == dirName);
-
-        if (newDir is null)
-        {
-            newDir = new Dir(dirName, _currentDir);
-            _currentDir.Dirs.Add(newDir);
-        }
-
-        return newDir;
-    }
-    
-    internal record Dir(string Name, IList<F> Files, IList<Dir> Dirs, Dir Parent)
-    {
-        public static readonly Dir Root = new("/", null);
-        public Dir(string name, Dir parent) : this(name, new List<F>(), new List<Dir>(), parent) { }
-        public int Size() => Files.Sum(x => x.Size) + Dirs.Sum(x => x.Size());
-    };
-
-    internal record F(string Name, int Size);
 }

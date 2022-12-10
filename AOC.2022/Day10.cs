89ca59fe7368f55c9b0e940c21062b65711fb33a
Thijs van der Vegt <497215@student.saxion.nl>
2022-12-10 23:36:40 +0100

    Day 10

--- AOC.2022/Day10.cs
+++ AOC.2022/Day10.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -7,71 +8,56 @@ namespace AOC._2022;
 public class Day10
 {
     private readonly List<string> _input = File.ReadAllLines("Inputs/Day10.txt").ToList();
-    private readonly HashSet<int> _interestingCycles = new() { 20, 60, 100, 140, 180, 220 };
-    private readonly HashSet<int> _rowCycles = new() { 40, 80, 120, 160, 200, 240 };
 
     [Test]
     public void Part1()
     {
-        int currentCycle = 0;
-        int currentValue = 1;
-        List<int> interestingValues = new List<int>();
-        
-
-        foreach (string line in _input)
-        {
-            if (line == "noop")
-            {
-                currentCycle += 1;
-                CheckInterestingCycle(currentCycle, interestingValues, currentValue);
-                
-                continue;
-            }
-
-            currentCycle += 1; // first cycle
-            CheckInterestingCycle(currentCycle, interestingValues, currentValue);
-            currentCycle += 1; // second cycle
-            CheckInterestingCycle(currentCycle, interestingValues, currentValue);
-            
-            currentValue += int.Parse(line.Split(" ")[1]);
-        }
-
-        interestingValues.Sum().Should().Be(12740);
+        HashSet<int> cycles = new() { 20, 60, 100, 140, 180, 220 };
+        Signal()
+            .Where(signal => cycles.Contains(signal.cycle))
+            .Select(signal => signal.x * signal.cycle)
+            .Sum()
+            .Should().Be(12740);
     }
 
     [Test]
     public void Part2()
     {
-        List<string> rows = new();
-        int currentCycle = 0;
-        int currentValue = 1;
-        int index = 0;
+        var screen = "";
+        foreach (var (cycle, spriteMiddle) in Signal()) {
+            var screenColumn = (cycle - 1) % 40;
 
-        foreach (string line in _input)
-        {
-            (int left, int middle, int right) sprite = (currentValue - 1, currentValue, currentValue + 1);
-            if (line == "noop")
-            {
-                currentCycle += 1;
+            screen += Math.Abs(spriteMiddle - screenColumn) < 2 ? "#" : " ";
 
-                continue;
+            if (screenColumn == 39) {
+                screen += "\n";
             }
-
-            currentCycle += 1; // first cycle
-
-            currentCycle += 1; // second cycle
-
-            
-            currentValue += int.Parse(line.Split(" ")[1]);
         }
+
+        screen.Should().Be("###  ###  ###   ##  ###   ##   ##  #### \n" + 
+                           "#  # #  # #  # #  # #  # #  # #  # #    \n" + 
+                           "#  # ###  #  # #  # #  # #  # #    ###  \n" +
+                           "###  #  # ###  #### ###  #### # ## #    \n" +
+                           "# #  #  # #    #  # # #  #  # #  # #    \n" +
+                           "#  # ###  #    #  # #  # #  #  ### #    \n");
     }
 
-    private void CheckInterestingCycle(int currentCycle, List<int> interestingValues, int currentValue)
+    private IEnumerable<(int cycle, int x)> Signal()
     {
-        if (_interestingCycles.Contains(currentCycle))
+        var (cycle, x) = (1, 1);
+        foreach (var parts in _input.Select(line => line.Split(" ")))
         {
-            interestingValues.Add(currentValue * currentCycle);
+            if (parts[0] == "noop")
+            {
+                yield return (cycle++, x);
+                continue;
+            }
+            
+            yield return (cycle++, x);
+            yield return (cycle++, x);
+            x += int.Parse(parts[1]);
         }
     }
     
 }
+

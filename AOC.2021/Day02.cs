6debe98057fc4079bbe2c1f6f92c1a00a749faaf
Thijs van der Vegt <497215@student.saxion.nl>
2022-12-23 23:38:22 +0100

    2021/02

---
+++ AOC.2021/Day02.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AOC._2021;
+
+[TestFixture]
+public class Day02
+{
+    private readonly string[] _input = File.ReadAllLines("Inputs/Day02.txt");
+    
+    [Test]
+    public void Part1()
+    {
+        Parse(_input)
+            .Aggregate(
+                new State1(0, 0),
+                (state, step) => step.dir switch
+                {
+                    'f' => state with { x = state.x + step.amount },
+                    'u' => state with { y = state.y - step.amount },
+                    'd' => state with { y = state.y + step.amount },
+                    _ => throw new Exception(),
+                },
+                res => res.x * res.y
+            )
+            .Should().Be(1654760);
+    }
+
+    [Test]
+    public void Part2()
+    {
+        Parse(_input)
+            .Aggregate(
+                new State2(0, 0, 0),
+                (state, step) => step.dir switch {
+                    'f' => state with { 
+                        x = state.x + step.amount, 
+                        y = state.y + step.amount * state.aim 
+                    },
+                    'u' => state with { aim = state.aim - step.amount },
+                    'd' => state with { aim = state.aim + step.amount },
+                    _ => throw new Exception(),
+                },
+                res => res.x * res.y
+            )
+            .Should().Be(1956047400);
+    }
+    
+    private static IEnumerable<Input> Parse(string[] input) =>
+        from line in input
+        let parts = line.Split()
+        select new Input(parts[0][0], int.Parse(parts[1]));
+    
+    private record Input(char dir, int amount);
+
+    private record State1(int x, int y);
+
+    private record State2(int x, int y, int aim);
+}

7556669eeaf68f52581d3300fd19cb5ca3b49f3e
Thijs van der Vegt <497215@student.saxion.nl>
2022-12-28 19:55:32 +0100

    2021/06

---
+++ AOC.2021/Day06.cs
@@ -0,0 +1,47 @@
+using System.Net;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AOC._2021;
+
+[TestFixture]
+public class Day06
+{
+    private readonly string _input = File.ReadAllText("Inputs/Day06.txt");
+
+    //       0123456           78 
+    //   ┌──[.......]─<─(+)───[..]──┐
+    //   └──────>────────┴─────>────┘
+    //     reproduction     newborn
+    
+    [Test]
+    public void Part1()
+    {
+        FishCountAfterNDays(_input, 80)
+            .Should().Be(391888);
+    }
+    
+    [Test]
+    public void Part2()
+    {
+        FishCountAfterNDays(_input, 256)
+            .Should().Be(1754597645339);
+    }
+    
+    private static long FishCountAfterNDays(string input, int N)
+    {
+        // group the fish by their timer, no need to deal with them one by one:
+        var fishCountByInternalTimer = new long[9];
+        foreach (var ch in input.Split(','))
+        {
+            fishCountByInternalTimer[int.Parse(ch)]++;
+        }
+
+        for (var n = 0; n < N; n++)
+        {
+            fishCountByInternalTimer[(n + 7) % 9] += fishCountByInternalTimer[n % 9];
+        }
+
+        return fishCountByInternalTimer.Sum();
+    }
+}

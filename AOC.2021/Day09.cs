68e2577c5a986576facab5101d01ff20031422a6
Thijs van der Vegt <497215@student.saxion.nl>
2022-12-30 18:17:39 +0100

    2021/09

---
+++ AOC.2021/Day09.cs
@@ -0,0 +1,81 @@
+using System.Collections.Immutable;
+using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AOC._2021;
+
+[TestFixture]
+public class Day09
+{
+    private readonly string[] _input = File.ReadAllLines("Inputs/Day09.txt");
+
+    [Test]
+    public void Part1()
+    {
+        var map = GetMap(_input);
+
+        GetLowPoints(map)
+            .Select(p => 1 + map[p]).Sum()
+            .Should().Be(554);
+    }
+
+    [Test]
+    public void Part2()
+    {
+        var map = GetMap(_input);
+
+        GetLowPoints(map)
+            .Select(p => BasinSize(map, p))
+            .OrderByDescending(basinSize => basinSize)
+            .Take(3)
+            .Aggregate(1, (m, basinSize) => m * basinSize)
+            .Should().Be(1017792);
+    }
+
+    private static ImmutableDictionary<Point, int> GetMap(string[] input)
+    {
+        var map = input;
+        return (
+            from y in Enumerable.Range(0, map.Length)
+            from x in Enumerable.Range(0, map[0].Length)
+            select new KeyValuePair<Point, int>(new Point(x, y), map[y][x] - '0')
+        ).ToImmutableDictionary();
+    }
+
+    private static IEnumerable<Point> Neighbours(Point point) =>
+        new[]
+        {
+            point with { Y = point.Y + 1 },
+            point with { Y = point.Y - 1 },
+            point with { X = point.X + 1 },
+            point with { X = point.X - 1 }
+        };
+
+    private static IEnumerable<Point> GetLowPoints(ImmutableDictionary<Point, int> map) =>
+        from point in map.Keys
+        // point is low if each of its neighbours is higher:
+        where Neighbours(point).All(neighbour => map[point] < map.GetValueOrDefault(neighbour, 9))
+        select point;
+
+    private static int BasinSize(ImmutableDictionary<Point, int> map, Point point)
+    {
+        // flood fill algorithm
+        var filled = new HashSet<Point> { point };
+        var queue = new Queue<Point>(filled);
+
+        while (queue.Any())
+        {
+            foreach (var neighbour in Neighbours(queue.Dequeue()).Except(filled))
+            {
+                if (map.GetValueOrDefault(neighbour, 9) != 9)
+                {
+                    queue.Enqueue(neighbour);
+                    filled.Add(neighbour);
+                }
+            }
+        }
+
+        return filled.Count;
+    }
+}

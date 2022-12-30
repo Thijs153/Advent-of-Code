ad0e1d827dea6503b3f34987714ec0d834625156
Thijs van der Vegt <497215@student.saxion.nl>
2022-12-30 17:17:01 +0100

    2021/07

---
+++ AOC.2021/Day07.cs
@@ -0,0 +1,50 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AOC._2021;
+
+[TestFixture]
+public class Day07
+{
+    private readonly string[] _input = File.ReadAllText("Inputs/Day07.txt").Split(",");
+
+    [Test]
+    public void Part1()
+    {
+        AlignSubmarines(Parse(_input).ToArray(), false)
+            .Min()
+            .Should().Be(348664);
+    }
+
+    [Test]
+    public void Part2()
+    {
+        AlignSubmarines(Parse(_input).ToArray(), true)
+            .Min()
+            .Should().Be(100220525);
+    }
+    
+    private static IEnumerable<int> AlignSubmarines(int[] numbers, bool expensiveFuelUsage)
+    {
+        for (int i = numbers.Min(); i <= numbers.Max(); i++)
+        {
+            int fuelUsage = 0;
+            foreach (var number in numbers)
+            {
+                int steps = Math.Abs(number - i);
+                
+                // 'expensiveFuelUsage' : 1 + 2 + ... n : n
+                fuelUsage += expensiveFuelUsage
+                    ? steps * (steps + 1) / 2
+                    : steps;
+            }
+
+            yield return fuelUsage;
+        }
+    }
+
+    private static IEnumerable<int> Parse(string[] input)
+    {
+        return input.Select(int.Parse);
+    }
+}

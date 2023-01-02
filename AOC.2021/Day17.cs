7b632ad98d3a9e12a48a49739dae517a438bb264
Thijs van der Vegt <497215@student.saxion.nl>
2023-01-02 19:57:28 +0100

    2021/17

---
+++ AOC.2021/Day17.cs
@@ -0,0 +1,64 @@
+using System.Text.RegularExpressions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AOC._2021;
+
+[TestFixture]
+public class Day17
+{
+    private readonly string _input = File.ReadAllText("Inputs/Day17.txt");
+
+    [Test]
+    public void Part1()
+    {
+        Solve(_input).Max()
+            .Should().Be(5995);
+    }
+
+    [Test]
+    public void Part2()
+    {
+        Solve(_input).Count()
+            .Should().Be(3202);
+    }
+    
+    private static IEnumerable<int> Solve(string input)
+    {
+        var m = Regex.Matches(input, "-?[0-9]+").Select(m => int.Parse(m.Value)).ToArray();
+
+        // Get the target rectangle
+        var (xMin, xMax) = (m[0], m[1]);
+        var (yMin, yMax) = (m[2], m[3]);
+
+        // Bounds for the initial horizontal and vertical speeds:
+        var vx0Min = 0; 
+        var vx0Max = xMax;  
+        var vy0Min = yMin;  
+        var vy0Max = -yMin;
+
+        // Run the simulation in the given bounds, maintaining maxY
+        for (var vx0 = vx0Min; vx0 <= vx0Max; vx0++) {
+            for (var vy0 = vy0Min; vy0 <= vy0Max; vy0++) {
+
+                var (x, y, vx, vy) = (0, 0, vx0, vy0);
+                var maxY = 0;
+                
+                while (x <= xMax && y >= yMin) {
+                   
+                    x += vx;
+                    y += vy;
+                    vy -= 1;
+                    vx = Math.Max(0, vx - 1);
+                    maxY = Math.Max(y, maxY);
+
+                    // if we are within target, yield maxY:
+                    if (x >= xMin && x <= xMax && y >= yMin && y <= yMax) {
+                        yield return maxY;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}

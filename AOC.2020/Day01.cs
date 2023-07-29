2aba0992ed31dab1e53f8e62818ad7ee4158e808
Thijs153 <497215@student.saxion.nl>
2023-07-29 18:21:01 +0200

    2020/01

---
+++ AOC.2020/Day01.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AOC._2020;
+
+[TestFixture]
+public class Day01
+{
+    private readonly string[] _input = File.ReadAllLines("Inputs/Day01.txt");
+
+    [Test]
+    public void Part1()
+    {
+        var numbers = Numbers(_input);
+        (
+            from x in numbers
+            let y = 2020 - x
+            where numbers.Contains(y)
+            select x * y
+        ).First().Should().Be(485739);
+    }
+
+    [Test]
+    public void Part2()
+    {
+        var numbers = Numbers(_input);
+        (
+            from x in numbers
+            from y in numbers
+            let z = 2020 - x - y
+            where numbers.Contains(z)
+            select x * y * z
+        ).First().Should().Be(161109702);
+    }
+
+    private static HashSet<int> Numbers(string[] input) => input.Select(int.Parse).ToHashSet();
+}

4ed62b68b92add9e25007cd31d61a97b71b48ee4
Thijs153 <497215@student.saxion.nl>
2023-07-30 14:18:48 +0200

    2020/03

---
+++ AOC.2020/Day03.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AOC._2020;
+
+[TestFixture]
+public class Day03
+{
+    private readonly string[] _input = File.ReadAllLines("Inputs/Day03.txt");
+
+    [Test]
+    public void Part1()
+    {
+        TreeCount(_input, (1, 3)).Should().Be(230);
+    }
+
+    [Test]
+    public void Part2()
+    {
+        TreeCount(_input, (1, 1), (1, 3), (1, 5), (1, 7), (2, 1)).Should().Be(9533698720L);
+    }
+
+    private static long TreeCount(string[] input, params (int drow, int dcol)[] slopes)
+    {
+        var (crow, ccol) = (input.Length, input[0].Length);
+        var mul = 1L;
+
+        foreach (var (drow, dcol) in slopes)
+        {
+            var (irow, icol) = (drow, dcol);
+            var trees = 0;
+            while (irow < crow)
+            {
+                if (input[irow][icol % ccol] == '#')
+                {
+                    trees++;
+                }
+
+                (irow, icol) = (irow + drow, icol + dcol);
+            }
+
+            mul *= trees;
+        }
+
+        return mul;
+    }
+}

4923b3ae2a08e12b56abe11100348244a1b31857
Thijs van der Vegt <t.vandervegt@innovadis.com>
2023-07-31 19:12:42 +0200

    2020/05

---
+++ AOC.2020/Day05.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AOC._2020;
+
+[TestFixture]
+public class Day05
+{
+    private readonly string _input = File.ReadAllText("Inputs/Day05.txt").ReplaceLineEndings("\n");
+
+    [Test]
+    public void Part1()
+    {
+        Seats(_input).Max().Should().Be(922);
+    }
+
+    [Test]
+    public void Part2()
+    {
+        var seats = Seats(_input);
+        var (min, max) = (seats.Min(), seats.Max());
+        
+        Enumerable.Range(min, max - min + 1).Single(id => !seats.Contains(id))
+            .Should().Be(747);
+    }
+    
+    private static HashSet<int> Seats(string input)
+    {
+        return input
+            .Replace("B", "1")
+            .Replace("F", "0")
+            .Replace("R", "1")
+            .Replace("L", "0")
+            .Split("\n")
+            .Select(row => Convert.ToInt32(row, 2))
+            .ToHashSet();
+    }
+}

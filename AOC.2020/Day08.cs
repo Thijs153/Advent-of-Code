7639b9bf11f111784bddd6d9f5d0487046339f67
Thijs153 <497215@student.saxion.nl>
2023-08-01 16:42:27 +0200

    2020/08

---
+++ AOC.2020/Day08.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AOC._2020;
+
+[TestFixture]
+public class Day08
+{
+    private readonly string[] _input = File.ReadAllLines("Inputs/Day08.txt");
+
+    [Test]
+    public void Part1()
+    {
+        Run(Parse(_input)).acc.Should().Be(1584);
+    }
+
+    [Test]
+    public void Part2()
+    {
+        Patches(Parse(_input))
+            .Select(Run)
+            .First(res => res.terminated).acc
+            .Should().Be(920);
+    }
+
+
+    private static (int acc, bool terminated) Run(Stm[] program)
+    {
+        var (ip, acc, seen) = (0, 0, new HashSet<int>());
+
+        while (true)
+        {
+            if (ip >= program.Length)
+            {
+                return (acc, true);
+            }  
+            if (seen.Contains(ip))
+            {
+                return (acc, false);
+            }
+
+            seen.Add(ip);
+            var stm = program[ip];
+            switch (stm.op)
+            {
+                case "nop": ip++;
+                    break;
+                case "acc": ip++;
+                    acc += stm.arg;
+                    break;
+                case "jmp": ip += stm.arg;
+                    break;
+            }
+        }
+    }
+
+    private static IEnumerable<Stm[]> Patches(Stm[] program)
+    {
+        return Enumerable.Range(0, program.Length)
+            .Where(line => program[line].op != "acc")
+            .Select(lineToPatch =>
+                program.Select((stm, line) =>
+                    line != lineToPatch ? stm :
+                    stm.op == "jmp" ? stm with { op = "nop" } :
+                    stm.op == "nop" ? stm with { op = "jmp" } :
+                    throw new Exception()
+                ).ToArray()
+            );
+    }
+
+    private static Stm[] Parse(string[] input) =>
+        input.Select(line => line.Split(" "))
+            .Select(parts => new Stm(parts[0], int.Parse(parts[1])))
+            .ToArray();
+    
+    private record Stm(string op, int arg);
+}

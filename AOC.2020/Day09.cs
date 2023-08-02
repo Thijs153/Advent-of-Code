3950ef5755af11ad7f9d9ed397984a75fe6e67de
Thijs153 <497215@student.saxion.nl>
2023-08-02 10:14:03 +0200

    2020/09

---
+++ AOC.2020/Day09.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AOC._2020;
+
+[TestFixture]
+public class Day09
+{
+    private readonly string[] _input = File.ReadAllLines("Inputs/Day09.txt");
+
+    [Test]
+    public void Part1()
+    {
+        FirstMisMatch(_input).Should().Be(105950735);
+    }
+
+    [Test]
+    public void Part2()
+    {
+        EncryptionWeakness(_input).Should().Be(13826915);
+    }
+
+    private static long FirstMisMatch(string[] input)
+    {
+        var numbers = input.Select(long.Parse).ToArray();
+
+        bool MisMatch(int i) => (
+            from j in Range(i - 25, i)
+            from k in Range(j + 1, i)
+            select numbers[j] + numbers[k]
+        ).All(sum => sum != numbers[i]);
+
+        return numbers[Range(25, input.Length).First(MisMatch)];
+    }
+
+    private static long EncryptionWeakness(string[] input)
+    {
+        var d = FirstMisMatch(input);
+        var lines = input.Select(long.Parse).ToList();
+
+        foreach (var j in Range(0, lines.Count))
+        {
+            var s = lines[j];
+            foreach (var k in Range(j + 1, lines.Count))
+            {
+                s += lines[k];
+                if (s > d)
+                {
+                    break;
+                }
+
+                if (s != d) continue;
+                
+                var range = lines.GetRange(j, k - j + 1);
+                return range.Min() + range.Max();
+            }
+        }
+
+        throw new Exception();
+    }
+    
+    private static IEnumerable<int> Range(int min, int lim) => Enumerable.Range(min, lim - min);
+}

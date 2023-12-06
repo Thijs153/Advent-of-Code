195637c6dfeea763b74709c2f005f7911e1c6cee
Thijs van der Vegt <t.vandervegt@innovadis.com>
2023-12-06 19:55:01 +0100

    2023/06 created files

---
+++ AOC.2023/Day06.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace AOC._2023;
+
+public class Day06
+{
+    private readonly string[] _input = File.ReadAllLines("Inputs/Day06.txt");
+
+    [Test]
+    public void Part1()
+    {
+        
+    }
+    
+    
+}

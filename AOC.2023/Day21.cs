c9ee783bc56b9067f7d4f77729af76128540b56b
Thijs153 <497215@student.saxion.nl>
2023-12-22 13:11:10 +0100

    2023/21 cleanup

--- AOC.2023/Day21.cs
+++ AOC.2023/Day21.cs
@@ -12,105 +12,78 @@ public class Day21
     public void Part1()
     {
         var map = ParseMap(_input);
-        var s = map.Keys.Single(k => map[k] == 'S');
-        var pos = new HashSet<Complex> { s };
+        var positions = new HashSet<Complex> { Center };
 
         for (var i = 0; i < 64; i++)
         {
-            pos = Step(map, pos);
+            positions = Step(map, positions);
         }
 
-        pos.Count.Should().Be(3764);
+        positions.Count.Should().Be(3764);
     }
 
     [Test]
     public void Part2()
     {
-        const int steps = 26501365;
-        const int loop = 260;
+        const int steps = 26501365; // 202300 * 131 + 65
+        const int bufferSize = 270; // anything that is > 260
+        var buffers = new Dictionary<Complex, CircularBuffer<long>>();
 
+        foreach (var entry in EntryPoints)
+        {
+            buffers[entry] = new CircularBuffer<long>(bufferSize);
+        }
+
+        buffers[Center][1] = 1;
+        for (var i = 1; i < steps; i++)
+        {
+            foreach (var cb in buffers.Values)
+            {
+                cb[^3] += cb[^1];
+            }
+
+            buffers[Center].Shift(0);
+            foreach (var item in Middles)
+            {
+                buffers[item].Shift(i % 131 == 65 ? 1 : 0);
+            }
+            foreach (var item in Corners)
+            {
+                buffers[item].Shift(i % 131 == 0 ? i / 131 : 0);
+            }
+        }
+        
         var map = ParseMap(_input);
-        var center = new Complex(65, 65);
-
-        Complex[] corners = [
-            new Complex(0, 0), new Complex(0, 130),
-            new Complex(130, 130), new Complex(130, 0),
-        ];
-
-        Complex[] middles = [
-            new Complex(65, 0), new Complex(65, 130),
-            new Complex(0, 65), new Complex(130, 65),
-        ];
-
-        var cohorts = new Dictionary<Complex, long[]>();
-        cohorts[center] = new long[loop + 1];
-        
-        foreach (var corner in corners)
-        {
-            cohorts[corner] = new long[loop + 1];
-        }
-
-        foreach (var middle in middles)
-        {
-            cohorts[middle] = new long[loop + 1];
-        }
-
-        var m = 0;
-        cohorts[center][m] = 1;
-        var phaseLength = loop + 1;
-        
-        for (var i = 1; i <= steps; i++)
-        {
-            var nextM = (m + phaseLength - 1) % phaseLength;
-            foreach (var item in cohorts.Keys)
-            {
-                var phase = cohorts[item];
-                var a = phase[(m + phase.Length - 1) % phase.Length];
-                var b = phase[(m + phase.Length - 2) % phase.Length];
-                var c = phase[(m + phase.Length - 3) % phase.Length];
-
-                phase[nextM] = 0;
-                phase[(nextM + phase.Length - 1) % phase.Length] = b;
-                phase[(nextM + phase.Length - 2) % phase.Length] = a + c;
-            }
-
-            m = nextM;
-
-            if (i >= 132 && (i - 132) % 131 == 0)
-            {
-                var newItems = i / 131;
-                foreach (var corner in corners)
-                {
-                    cohorts[corner][m] += newItems;
-                }
-            } else if (i >= 66 && (i - 66) % 131 == 0)
-            {
-                foreach (var middle in middles)
-                {
-                    cohorts[middle][m]++;
-                }
-            }
-        }
-
         var res = 0L;
 
-        foreach (var item in cohorts.Keys)
+        foreach (var entry in EntryPoints)
         {
-            var phase = cohorts[item];
-            var pos = new HashSet<Complex> { item };
-
-            for (var i = 0; i < phase.Length; i++)
+            var positions = new HashSet<Complex> { entry };
+            for (var i = 0; i < bufferSize; i++)
             {
-                var count = phase[(m + i) % phase.Length];
-                res += pos.Count * count;
-                pos = Step(map, pos);
+                res += positions.Count * buffers[entry][i];
+                positions = Step(map, positions);
             }
         }
 
         res.Should().Be(622926941971282);
     }
-    
 
+    private static readonly Complex Center = new Complex(65, 65);
+    
+    private static readonly Complex[] Corners = [
+        new Complex(0, 0), new Complex(0, 130),
+        new Complex(130, 130), new Complex(130, 0),
+    ];
+
+    private static readonly Complex[] Middles = [
+        new Complex(65, 0), new Complex(65, 130),
+        new Complex(0, 65), new Complex(130, 65),
+    ];
+
+    private static readonly Complex[] EntryPoints = [Center, ..Corners, ..Middles];
+    private static readonly Complex[] Dirs = [1, -1, Complex.ImaginaryOne, -Complex.ImaginaryOne];
+    
     private static HashSet<Complex> Step(Dictionary<Complex, char> map, HashSet<Complex> pos)
     {
         var res = new HashSet<Complex>();
@@ -136,4 +109,25 @@ public class Day21
             new Complex(iCol, iRow), input[iRow][iCol]
         )
     ).ToDictionary();
+
+    private class CircularBuffer<T>(int size)
+    {
+        public int Count => size;
+        private readonly T[] _items = new T[size];
+        private int _i;
+
+        public T this[int index]
+        {
+            get => _items[(_i + index) % size];
+            set => _items[(_i + index) % size] = value;
+        }
+
+        public T Shift(T t)
+        {
+            _i = (_i + size - 1) % size;
+            var res = _items[_i];
+            _items[_i] = t;
+            return res;
+        }
+    }
 }
